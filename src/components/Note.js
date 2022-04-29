8e83a4e84f21bd133524e91d981a499dcab844b8
maxlestage <maxlestage@icloud.com>
2022-04-29 14:46:34 +0200

    Ajout d'un bouton pour suprr une note

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,13 +1,13 @@
-const Note = ({ note, toggleImportance }) => {
-  const label = note.important
-    ? 'make not important' : 'make important'
+const Note = ({ note, toggleImportance, remove }) => {
+  const label = note.important ? "make not important" : "make important";
 
   return (
     <li className="note">
-      {note.content} 
+      {note.content}
+      <button onClick={remove}>Delete</button>
       <button onClick={toggleImportance}>{label}</button>
     </li>
-  )
-}
+  );
+};
 
-export default Note
+export default Note;
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,20 +1,29 @@
 import axios from "axios";
 // const baseUrl = 'http://localhost:3001/notes'
-const baseUrl = "/api/notes";
+const baseUrl = "https://fsopenchap3.herokuapp.com/api/notes";
 
-const getAll = () => {
+const getAll = async () => {
   const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+  const response = await request;
+  return response.data;
 };
 
-const create = (newObject) => {
+const create = async (newObject) => {
   const request = axios.post(baseUrl, newObject);
-  return request.then((response) => response.data);
+  const response = await request;
+  return response.data;
 };
 
-const update = (id, newObject) => {
+const update = async (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
+  const response = await request;
+  return response.data;
 };
 
-export default { getAll, create, update };
+const remove = async (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  const response = await request;
+  return response.data;
+};
+
+export default { getAll, create, update, remove };
